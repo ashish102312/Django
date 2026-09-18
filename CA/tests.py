010758b6f1482d94b9aa7472190af5a9cf6a93bb
Ashish Bhardwaj <aash447700@gmail.com>
2026-09-18 12:39:35 +0530

    CA/tests.py

--- CA/tests.py
+++ CA/tests.py
@@ -1,9 +1,12 @@
-# from django.test import TestCase
+from django.test import TestCase
 
+class CaFilterViewTests(TestCase):
+     def test_filter_uses_query_parameters(self):
+         response = self.client.get('/filter/?genre=action&year=2025')
+         self.assertEqual(response.status_code, 200)
+         self.assertContains(response, 'action')
+         self.assertContains(response, '2025')
 
-# class CaFilterViewTests(TestCase):
-#     def test_filter_uses_query_parameters(self):
-#         response = self.client.get('/filter/?genre=action&year=2025')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertContains(response, 'action')
-#         self.assertContains(response, '2025')
+class HomeTest(TestCase):
+     def test_home(self):
+          self.assertEqual(2+2,4)
